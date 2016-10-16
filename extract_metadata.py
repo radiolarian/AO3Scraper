556b80946e6cf0dc4b960c33e1a2764cf940d91b
Sarah Sterman <ssterman@berkeley.edu>
2016-10-15 22:05:31 -0700

    script to make fic csv smaller by separating out metadata

---
+++ extract_metadata.py
@@ -0,0 +1,24 @@
+'''
+Sometimes the resulting files are huge.  To extract the metadata without the fics, use this. 
+'''
+
+import csv
+import os
+
+def main():
+	csv.field_size_limit(1000000000)  # up the field size because stories are long
+	csv_name = raw_input("What is the csv called?  ")
+	
+	# clean extension
+	if ".csv" not in csv_name:
+		csv_name = csv_name + ".csv"
+
+	with open(csv_name, 'rb') as csvfile:
+		with open(csv_name[:-4] + "_metadata.csv", 'a') as metacsv:
+			rd = csv.reader(csvfile, delimiter=',', quotechar='"')
+			wr = csv.writer(metacsv, delimiter=',', quotechar='"')
+			for row in rd:
+				work_id = row[0]
+				wr.writerow(row[:-1])
+
+main()
