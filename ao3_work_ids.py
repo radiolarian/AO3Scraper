15c7e6d6044f3a789449220b009ce4d4d199f3d9
Sarah Sterman <ssterman@berkeley.edu>
2017-06-11 14:05:01 -0700

    indent with spaces

--- ao3_work_ids.py
+++ ao3_work_ids.py
@@ -26,43 +26,43 @@ csv_name = ""
 # what to call the output csv
 # 
 def get_user_params():
-	global url
-	global csv_name
-	global num_requested_fic
-	# user input the url
-	while (url == ""):
-		url = raw_input("What URL should we scrape? ")
+    global url
+    global csv_name
+    global num_requested_fic
+    # user input the url
+    while (url == ""):
+        url = raw_input("What URL should we scrape? ")
 
-	# how many fic?
-	nqf = ""
-	while (nqf == ""):
-		nqf = raw_input("How many fic do you want? (for all, enter 'a')  " )
+    # how many fic?
+    nqf = ""
+    while (nqf == ""):
+        nqf = raw_input("How many fic do you want? (for all, enter 'a')  " )
 
-	if nqf == "a":
-		num_requested_fic = -1
-	else:
-		num_requested_fic = int(nqf)
+    if nqf == "a":
+        num_requested_fic = -1
+    else:
+        num_requested_fic = int(nqf)
 
-	while (csv_name == ""):
-		csv_name = raw_input("What should we call the output csv? ")
+    while (csv_name == ""):
+        csv_name = raw_input("What should we call the output csv? ")
 
 # 
 # navigate to a works listed page,
 # then extract all work ids
 # 
 def get_ids():
-	req = requests.get(url)
-	soup = BeautifulSoup(req.text, "lxml")
-	# some responsiveness in the "UI"
-	sys.stdout.write('.')
-	sys.stdout.flush()
-	works = soup.find_all(class_="work blurb group")
-	ids = []
-	for tag in works:
-		t = tag.get('id')
-		t = t[5:]
-		ids.append(t)
-	return ids
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    # some responsiveness in the "UI"
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    works = soup.find_all(class_="work blurb group")
+    ids = []
+    for tag in works:
+        t = tag.get('id')
+        t = t[5:]
+        ids.append(t)
+    return ids
 
 # 
 # update the url to move to the next page
@@ -70,32 +70,32 @@ def get_ids():
 # but there will be no works listed
 # 
 def update_url_to_next_page():
-	global url
-	key = "page="
-	start = url.find(key)
+    global url
+    key = "page="
+    start = url.find(key)
 
-	# there is already a page indicator in the url
-	if (start is not -1):
-		# find where in the url the page indicator starts and ends
-		page_start_index = start + len(key)
-		page_end_index = url.find("&", page_start_index)
-		# if it's in the middle of the url
-		if (page_end_index is not -1):
-			page = int(url[page_start_index:page_end_index]) + 1
-			url = url[:page_start_index] + str(page) + url[page_end_index:]
-		# if it's at the end of the url
-		else:
-			page = int(url[page_start_index:]) + 1
-			url = url[:page_start_index] + str(page)
+    # there is already a page indicator in the url
+    if (start is not -1):
+        # find where in the url the page indicator starts and ends
+        page_start_index = start + len(key)
+        page_end_index = url.find("&", page_start_index)
+        # if it's in the middle of the url
+        if (page_end_index is not -1):
+            page = int(url[page_start_index:page_end_index]) + 1
+            url = url[:page_start_index] + str(page) + url[page_end_index:]
+        # if it's at the end of the url
+        else:
+            page = int(url[page_start_index:]) + 1
+            url = url[:page_start_index] + str(page)
 
-	# there is no page indicator, so we are on page 1
-	else:
-		# there are other modifiers
-		if (url.find("?") is not -1):
-			url = url + "&page=2"
-		# there an no modifiers yet
-		else:
-			url = url + "?page=2"
+    # there is no page indicator, so we are on page 1
+    else:
+        # there are other modifiers
+        if (url.find("?") is not -1):
+            url = url + "&page=2"
+        # there an no modifiers yet
+        else:
+            url = url + "?page=2"
 
 
 # 
@@ -105,15 +105,15 @@ def update_url_to_next_page():
 # so an interrupted search can be restarted
 # 
 def write_ids_to_csv(ids):
-	global num_recorded_fic
-	with open(csv_name + ".csv", 'a') as csvfile:
-	    wr = csv.writer(csvfile, delimiter=',')
-	    for id in ids:
-			if (not_finished()):
-				wr.writerow([id, url])
-				num_recorded_fic = num_recorded_fic + 1
-			else:
-				break
+    global num_recorded_fic
+    with open(csv_name + ".csv", 'a') as csvfile:
+        wr = csv.writer(csvfile, delimiter=',')
+        for id in ids:
+            if (not_finished()):
+                wr.writerow([id, url])
+                num_recorded_fic = num_recorded_fic + 1
+            else:
+                break
 
 # 
 # if you want everything, you're not done
@@ -121,36 +121,36 @@ def write_ids_to_csv(ids):
 # recorded doesn't update until it's actually written to the csv
 # 
 def not_finished():
-	if (num_requested_fic == -1):
-		return True
-	else:
-		if (num_recorded_fic < num_requested_fic):
-			return True
-		else:
-			return False
+    if (num_requested_fic == -1):
+        return True
+    else:
+        if (num_recorded_fic < num_requested_fic):
+            return True
+        else:
+            return False
 
 # 
 # include a text file with the starting url,
 # and the number of requested fics
 # 
 def make_readme():
-	with open(csv_name + "_readme.txt", "w") as text_file:
-		text_file.write("url: " + url + "\n" + "num_requested_fic: " + str(num_requested_fic) + "\n" + "retreived on: " + str(datetime.datetime.now()))
+    with open(csv_name + "_readme.txt", "w") as text_file:
+        text_file.write("url: " + url + "\n" + "num_requested_fic: " + str(num_requested_fic) + "\n" + "retreived on: " + str(datetime.datetime.now()))
 
 
 def main():
-	get_user_params()
-	make_readme()
-	while(not_finished()):
-		# 5 second delay between requests as per AO3's terms of service
-		time.sleep(5)
-		ids = get_ids()
-		# if the current page is empty, you've run out of fic
-		if (len(ids) is 0):
-			break
-		write_ids_to_csv(ids)
-		update_url_to_next_page()
-	print "That's all, folks"
+    get_user_params()
+    make_readme()
+    while(not_finished()):
+        # 5 second delay between requests as per AO3's terms of service
+        time.sleep(5)
+        ids = get_ids()
+        # if the current page is empty, you've run out of fic
+        if (len(ids) is 0):
+            break
+        write_ids_to_csv(ids)
+        update_url_to_next_page()
+    print "That's all, folks"
 
 
 main()
